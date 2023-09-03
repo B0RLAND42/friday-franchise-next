3063859b071b413d07c7eb5804f9d054da355a0d
B0RLAND42 <mike.lapointe85@gmail.com>
2023-09-04 02:03:31 +1000

    Updated responsiveness

--- src/components/layout/Footer/Footer.jsx
+++ src/components/layout/Footer/Footer.jsx
@@ -11,28 +11,20 @@ function Footer() {
     <footer className={styles.footer}>
       <div className={styles.brand}>
         <Link href='/' >
-          {/* <span className={styles.logoEnds}>&lt; </span>  */}
           <span className={styles.logoMain}>
           &copy; {getCurrentYear()} Friday The 13th FanSite
         </span>
-          {/* <span className={styles.logoEnds}>&#47;&gt;</span> */}
         </Link>
       </div>
       <nav>
         <ul>
           <li>
-            <Link href='/terms' className={styles.blend}>Terms of Use </Link>
-          </li>
-          <li>
-            <Link href='/contact' className={styles.blend}>Contact Us </Link>
-          </li>
-          <li>
             <Link href='/about' className={styles.blend}>About Friday The 13th FanSite </Link>
           </li>
         </ul>
       </nav>
-        <a href="https://github.com/B0RLAND42" target="_blank" rel="noreferrer">
-          <FaGithub size={30} style={{ color: "#FFFFFF", marginLeft: "2.4rem" }}/>
+        <a href="https://github.com/B0RLAND42" className={styles.gitIcon} target="_blank" rel="noreferrer">
+          <FaGithub size={30} style={{ color: "#FFFFFF" }}/>
         </a>
     </footer>
   );
