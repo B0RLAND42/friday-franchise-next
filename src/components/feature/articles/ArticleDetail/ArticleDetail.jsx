75d36a6411d601481a3bd6cea0434811517388b4
B0RLAND42 <mike.lapointe85@gmail.com>
2026-02-14 19:27:00 +1100

    updates
    made cards full bg image instead of half bottom grey

--- src/components/feature/articles/ArticleDetail/ArticleDetail.jsx
+++ src/components/feature/articles/ArticleDetail/ArticleDetail.jsx
@@ -1,54 +1,47 @@
 import styles from "./ArticleDetail.module.scss";
 import Image from "next/image";
 import Card from "@/components/common/Card";
-import Button from "@/components/common/Button/Button";
-import ButtonLink from "@/components/common/ButtonLink";
 import { useRouter } from "next/router";
 
 function ArticleDetail(props) {
   const router = useRouter();
 
   function handleNavigateBack() {
-    router.push("/fridaymovies"); // or "/docs" depending on your app
+    router.push("/fridaymovies");
   }
 
   return (
     <div className={styles.back}>
       <Card size="single">
-        {/* CARD MAIN */}
         <div className={styles.articleMain}>
+          {/* Image as full background */}
           <div className={styles.imageBox}>
             <Image
               className={styles.nextImage}
               src={props.image}
               alt={props.title}
-              fill={true}
+              fill
               sizes="100vw"
-              priority={true}
+              priority
             />
+            {/* Optional overlay for text readability */}
+            <div className={styles.overlay}></div>
+
+            {/* Text on top of image */}
+            <div className={styles.articleContent}>
+              <h1>{props.title}</h1>
+              <h3>
+                <span>Director</span> {props.director}
+              </h3>
+              <h3>
+                <span>Release Date</span> {props.year}
+              </h3>
+              <p>{props.description}</p>
+              <button type="button" onClick={handleNavigateBack}>
+                BACK
+              </button>
+            </div>
           </div>
-          <div className={styles.articleContent}>
-            <h1>{props.title}</h1>
-            <h3>
-              <span>Director</span> {props.director}
-            </h3>
-            <h3>
-              <span>Release Date</span> {props.year}
-            </h3>
-            <p>{props.description}</p>
-          </div>
-        </div>
-        {/* CARD FOOTER */}
-        <div className={styles.articleFooter}>
-          {props.url ? (
-            <ButtonLink path={props.url} target="_blank">
-              BACK
-            </ButtonLink>
-          ) : (
-            <Button type="button" onClick={handleNavigateBack}>
-              BACK
-            </Button>
-          )}
         </div>
       </Card>
     </div>
