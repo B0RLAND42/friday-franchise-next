0f4a44a77485d6d2cc876b2ff98402ac071d6dc2
B0RLAND42 <mike.lapointe85@gmail.com>
2026-02-14 20:23:31 +1100

    footer
    changed gradient bg color, uppercase text on mobile

--- src/components/layout/Footer/Footer.jsx
+++ src/components/layout/Footer/Footer.jsx
@@ -12,7 +12,7 @@ function Footer() {
         <Link href='/' className={styles.logoMain}>
           &copy; {getCurrentYear()} Friday The 13th FanSite
         </Link>
-        <Link href='/about' className={styles.blend}>About Friday The 13th FanSite </Link>
+        <Link href='/about' className={styles.blend}>ABOUT FRIDAY THE 13TH FANSITE </Link>
         <a href="https://github.com/B0RLAND42"  target="_blank" rel="noreferrer">
           <FaGithub size={30} style={{ marginTop: '6px' }}/>
         </a>
