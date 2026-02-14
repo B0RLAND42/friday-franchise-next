01a47fc4e3e342999509969bf0094da61a506b92
B0RLAND42 <mike.lapointe85@gmail.com>
2026-02-14 19:09:39 +1100

    updates
    made the movie cards clickable instead of using the out-dated buttons on bottom of cards

--- src/components/feature/articles/ArticleItem/ArticleItem.jsx
+++ src/components/feature/articles/ArticleItem/ArticleItem.jsx
@@ -1,54 +1,46 @@
 import styles from './ArticleItem.module.scss';
 import Image from 'next/image';
 import Card from '@/components/common/Card';
-import Button from '@/components/common/Button/Button';
-import ButtonLink from '@/components/common/ButtonLink';
 import { useRouter } from 'next/router';
 
 function ArticleItem(props) {
   const router = useRouter();
 
-  function handleNavigate(){
+  function handleNavigate() {
     router.push(router.pathname + '/' + props.id);
   }
 
+  // Determine image source
+  const imageSrc = props.image || props.urlToImage;
+
   return (
     <Card size="grid">
-      {/* CARD MAIN */}
       <div className={styles.articleMain}>
-        <div className={styles.imageBox}>
-          { props.image ?
-            <Image 
+        {/* Make the image clickable */}
+        <div
+          className={styles.imageBox}
+          onClick={handleNavigate}
+          style={{ cursor: 'pointer' }}
+        >
+          {imageSrc && (
+            <Image
               className={styles.nextImage}
-              src={props.image} 
-              alt={props.title} 
-              fill={true}
-              sizes={"(max-width: 768px) 100vw, 50vw"}
-            /> :
-            <Image 
-              className={styles.nextImage}
-              src={props.urlToImage} 
-              alt={props.title} 
-              fill={true}
-              sizes={"(max-width: 768px) 100vw, 50vw"}
+              src={imageSrc}
+              alt={props.title || 'Article Image'}
+              fill
+              sizes="(max-width: 768px) 100vw, 50vw"
             />
-          } 
+          )}
         </div>
-        <div className={styles.articleContent}>
+
+        {/* Remove the title */}
+        {/* <div className={styles.articleContent}>
           <h2>{props.title}</h2>
-        </div>
+        </div> */}
       </div>
-      {/* CARD FOOTER */}
-      <div className={styles.articleFooter}>
-        { props.url ? 
-          <ButtonLink path={props.url} target="_blank">
-            More Info
-          </ButtonLink> :
-          <Button onClick={handleNavigate}>
-            More Info
-          </Button>
-        }
-      </div>
+
+      {/* Remove the footer button entirely */}
+      {/* <div className={styles.articleFooter}></div> */}
     </Card>
   );
 }
